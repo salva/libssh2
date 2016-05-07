d890d885e405cf9443db9c02ebe201e9067605ad
Salvador Fandiño <sfandino@yahoo.com>
2016-05-07 16:51:10 +0200

    libssh2_channel_send_eof: use read_avail to check if data is queued

--- src/channel.c
+++ src/channel.c
@@ -2140,26 +2140,10 @@ libssh2_channel_send_eof(LIBSSH2_CHANNEL *channel)
 LIBSSH2_API int
 libssh2_channel_eof(LIBSSH2_CHANNEL * channel)
 {
-    LIBSSH2_SESSION *session;
-    LIBSSH2_PACKET *packet;
-
     if(!channel)
         return LIBSSH2_ERROR_BAD_USE;
 
-    session = channel->session;
-    packet = _libssh2_list_first(&session->packets);
-
-    while (packet) {
-        if (((packet->data[0] == SSH_MSG_CHANNEL_DATA)
-             || (packet->data[0] == SSH_MSG_CHANNEL_EXTENDED_DATA))
-            && (channel->local.id == _libssh2_ntohu32(packet->data + 1))) {
-            /* There's data waiting to be read yet, mask the EOF status */
-            return 0;
-        }
-        packet = _libssh2_list_next(&packet->node);
-    }
-
-    return channel->remote.eof;
+    return (!channel->read_avail && channel->remote.eof);
 }
 
 /*
